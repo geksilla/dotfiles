9e401c36251542ac7e7ab38ff96afbbd83fab172
geksilla <buzhordenis@gmail.com>
2015-03-10 23:35:49 +0200

    Added org-bullets mode.

--- emacs/setup/init-org.el
+++ emacs/setup/init-org.el
@@ -1,7 +1,11 @@
+(require-package 'org-bullets)
+
 (org-babel-do-load-languages
  'org-babel-load-languages
  '(
    (plantuml .t)))
 (setq org-plantuml-jar-path
       (expand-file-name "~/scripts/plantuml.jar"))
+(add-hook 'org-mode-hook 'org-bullets-mode)
+
 (provide 'init-org)
